815ad1e57a6f812c1d24cee93c93cf080c2162c4
Martin Nečas <necas.marty@gmail.com>
2022-11-02 15:16:41 +0100

    Add ovirt/ovirt-imageio image

--- ovirt/setup.sh
+++ ovirt/setup.sh
@@ -12,5 +12,9 @@ kubectl apply -f ovirt/fakeovirt_deployment.yml
 while ! kubectl get deployment -n konveyor-forklift fakeovirt; do sleep 10; done
 kubectl wait deployment -n konveyor-forklift fakeovirt --for condition=Available=True --timeout=180s
 
+kubectl apply -f ovirt-imageio/imageio_deployment.yml
+while ! kubectl get deployment -n konveyor-forklift imageio; do sleep 10; done
+kubectl wait deployment -n konveyor-forklift imageio --for condition=Available=True --timeout=180s
+
 kubectl apply -f ovirt/forklift_provider.yml
 kubectl apply -f ovirt/forklift_migration.yml
