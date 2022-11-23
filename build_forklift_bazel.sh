0c11c3eb752e0b3a7b539aa85cbeacc3c0864c45
Evgeny Slutsky <eslutsky@redhat.com>
2022-11-23 12:23:33 +0200

    add stub image for virt-v2v
    we dont wish to run the real virt-v2v during CI testing.
    
    Signed-off-by: Evgeny Slutsky <eslutsky@redhat.com>

--- build_forklift_bazel.sh
+++ build_forklift_bazel.sh
@@ -4,6 +4,9 @@ echo "Running $0"
 # Change the dir to FORKLIFT_DIR (default forklift)
 cd ${FORKLIFT_DIR:-forklift}
 
+# Copy the stub-images under the bazel workspace
+cp -fr ../stub-images .
+
 export REGISTRY=localhost:5001
 export REGISTRY_TAG=latest
 export REGISTRY_ACCOUNT=""
@@ -11,5 +14,6 @@ export REGISTRY_ACCOUNT=""
 bazel run push-forklift-controller
 bazel run push-forklift-validation
 bazel run push-forklift-operator
-bazel run push-forklift-operator-bundle --action_env CONTROLLER_IMAGE=${REGISTRY}/forklift-controller:${REGISTRY_TAG} --action_env VALIDATION_IMAGE=${REGISTRY}/forklift-validation:${REGISTRY_TAG} --action_env OPERATOR_IMAGE=${REGISTRY}/forklift-operator:${REGISTRY_TAG}
+bazel run push-forklift-operator-bundle --action_env CONTROLLER_IMAGE=${REGISTRY}/forklift-controller:${REGISTRY_TAG} --action_env VALIDATION_IMAGE=${REGISTRY}/forklift-validation:${REGISTRY_TAG} --action_env OPERATOR_IMAGE=${REGISTRY}/forklift-operator:${REGISTRY_TAG} --action_env VIRT_V2V_IMAGE=${REGISTRY}/forklift-virt-v2v-stub:${REGISTRY_TAG}
 bazel run push-forklift-operator-index --action_env REGISTRY=${REGISTRY} --action_env REGISTRY_TAG=${REGISTRY_TAG} --action_env OPM_OPTS="--use-http"
+bazel run //stub-images:push-forklift-virt-v2v-stub
