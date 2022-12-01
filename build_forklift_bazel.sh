0f58c1e9419d1ae1e8a5df9b49975d66137d926f
Evgeny Slutsky <eslutsky@redhat.com>
2022-12-01 16:20:21 +0200

    avoing building the test images on each run
    Signed-off-by: Evgeny Slutsky <eslutsky@redhat.com>

--- build_forklift_bazel.sh
+++ build_forklift_bazel.sh
@@ -22,12 +22,5 @@ export REGISTRY_ACCOUNT=""
 bazel run push-forklift-controller
 bazel run push-forklift-validation
 bazel run push-forklift-operator
-bazel run push-forklift-operator-bundle --action_env CONTROLLER_IMAGE=${REGISTRY}/forklift-controller:${REGISTRY_TAG} --action_env VALIDATION_IMAGE=${REGISTRY}/forklift-validation:${REGISTRY_TAG} --action_env OPERATOR_IMAGE=${REGISTRY}/forklift-operator:${REGISTRY_TAG} --action_env VIRT_V2V_IMAGE=${REGISTRY}/forklift-virt-v2v-stub:${REGISTRY_TAG}
+bazel run push-forklift-operator-bundle --action_env CONTROLLER_IMAGE=${REGISTRY}/forklift-controller:${REGISTRY_TAG} --action_env VALIDATION_IMAGE=${REGISTRY}/forklift-validation:${REGISTRY_TAG} --action_env OPERATOR_IMAGE=${REGISTRY}/forklift-operator:${REGISTRY_TAG} --action_env VIRT_V2V_IMAGE=quay.io/kubev2v/forklift-virt-v2v-stub:${REGISTRY_TAG}
 bazel run push-forklift-operator-index --action_env REGISTRY=${REGISTRY} --action_env REGISTRY_TAG=${REGISTRY_TAG} --action_env OPM_OPTS="--use-http"
-
-[ ! -e "${SCRIPT_DIR}/stub-images" ] && { echo "stub-images not found";exit 2; }
-
-# Copy the stub-images under the bazel workspace
-cp -fr ${SCRIPT_DIR}/stub-images .
-bazel run //stub-images:push-vddk-test-vmdk
-bazel run //stub-images:push-forklift-virt-v2v-stub
