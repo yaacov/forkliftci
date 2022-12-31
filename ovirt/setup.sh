f81fa0d5a382bbe0159b0a0546e341fe3b3d3c27
Arik Hadas <ahadas@redhat.com>
2022-12-31 07:07:37 +0000

    Skip customizing ovirt-imageio
    The customization was moved to:
    https://github.com/kubev2v/ovirt-imageio-server
    
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- ovirt/setup.sh
+++ ovirt/setup.sh
@@ -4,14 +4,11 @@ set -ex
 
 kubectl apply -f ovirt/patch.yml
 
-docker build ovirt-imageio/ -t localhost:5001/imageio
-docker push localhost:5001/imageio
-
 kubectl apply -f ovirt/fakeovirt_deployment.yml
 while ! kubectl get deployment -n konveyor-forklift fakeovirt; do sleep 10; done
 kubectl wait deployment -n konveyor-forklift fakeovirt --for condition=Available=True --timeout=180s
 
-kubectl apply -f ovirt-imageio/imageio_deployment.yml
+kubectl apply -f ovirt/imageio_deployment.yml
 while ! kubectl get deployment -n konveyor-forklift imageio; do sleep 10; done
 kubectl wait deployment -n konveyor-forklift imageio --for condition=Available=True --timeout=180s
 
