ce26d0957467b8e802fdafe6a3e03828f2412266
Arik Hadas <ahadas@redhat.com>
2022-12-31 07:23:59 +0000

    Deploy fakeovirt and imageio simultaneously
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- ovirt/setup.sh
+++ ovirt/setup.sh
@@ -3,12 +3,12 @@
 set -ex
 
 kubectl apply -f ovirt/patch.yml
-
 kubectl apply -f ovirt/fakeovirt_deployment.yml
+kubectl apply -f ovirt/imageio_deployment.yml
+
 while ! kubectl get deployment -n konveyor-forklift fakeovirt; do sleep 10; done
 kubectl wait deployment -n konveyor-forklift fakeovirt --for condition=Available=True --timeout=180s
 
-kubectl apply -f ovirt/imageio_deployment.yml
 while ! kubectl get deployment -n konveyor-forklift imageio; do sleep 10; done
 kubectl wait deployment -n konveyor-forklift imageio --for condition=Available=True --timeout=180s
 
