7a95d3d8386352925593eeb32acb91749e436ca6
Arik Hadas <ahadas@redhat.com>
2022-12-31 07:26:41 +0000

    Rename imageio to ovirt-imageio
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- ovirt/setup.sh
+++ ovirt/setup.sh
@@ -4,12 +4,12 @@ set -ex
 
 kubectl apply -f ovirt/patch.yml
 kubectl apply -f ovirt/fakeovirt_deployment.yml
-kubectl apply -f ovirt/imageio_deployment.yml
+kubectl apply -f ovirt/ovirt_imageio_deployment.yml
 
 while ! kubectl get deployment -n konveyor-forklift fakeovirt; do sleep 10; done
 kubectl wait deployment -n konveyor-forklift fakeovirt --for condition=Available=True --timeout=180s
 
-while ! kubectl get deployment -n konveyor-forklift imageio; do sleep 10; done
-kubectl wait deployment -n konveyor-forklift imageio --for condition=Available=True --timeout=180s
+while ! kubectl get deployment -n konveyor-forklift ovirt-imageio; do sleep 10; done
+kubectl wait deployment -n konveyor-forklift ovirt-imageio --for condition=Available=True --timeout=180s
 
 . ovirt/e2e_env_vars.sh
