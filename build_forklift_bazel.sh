2ce14197adf749f8a246fa253294bc3bebd4ccc9
Evgeny Slutsky <eslutsky@redhat.com>
2023-01-11 17:21:25 +0200

    dont request KVM Flags for vsphere virt-v2v

--- build_forklift_bazel.sh
+++ build_forklift_bazel.sh
@@ -23,5 +23,5 @@ bazel run push-forklift-api
 bazel run push-forklift-controller
 bazel run push-forklift-validation
 bazel run push-forklift-operator
-bazel run push-forklift-operator-bundle --action_env CONTROLLER_IMAGE=${REGISTRY}/forklift-controller:${REGISTRY_TAG} --action_env VALIDATION_IMAGE=${REGISTRY}/forklift-validation:${REGISTRY_TAG} --action_env OPERATOR_IMAGE=${REGISTRY}/forklift-operator:${REGISTRY_TAG} --action_env VIRT_V2V_IMAGE=quay.io/kubev2v/forklift-virt-v2v-stub:${REGISTRY_TAG} --action_env API_IMAGE=${REGISTRY}/forklift-api:${REGISTRY_TAG}
+bazel run push-forklift-operator-bundle --action_env CONTROLLER_IMAGE=${REGISTRY}/forklift-controller:${REGISTRY_TAG} --action_env VALIDATION_IMAGE=${REGISTRY}/forklift-validation:${REGISTRY_TAG} --action_env OPERATOR_IMAGE=${REGISTRY}/forklift-operator:${REGISTRY_TAG} --action_env VIRT_V2V_IMAGE=quay.io/kubev2v/forklift-virt-v2v-stub:${REGISTRY_TAG} --action_env API_IMAGE=${REGISTRY}/forklift-api:${REGISTRY_TAG} --action_env VIRT_V2V_DONT_REQUEST_KVM=true
 bazel run push-forklift-operator-index --action_env REGISTRY=${REGISTRY} --action_env REGISTRY_TAG=${REGISTRY_TAG} --action_env OPM_OPTS="--use-http"
