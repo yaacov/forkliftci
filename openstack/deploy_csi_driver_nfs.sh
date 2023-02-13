9773d05259c58bcaf1f6dce401a14a9cd9b1d323
Evgeny Slutsky <eslutsky@redhat.com>
2023-02-13 17:07:09 +0200

    introduce csi-driver-nfs for openstack volumes
    Signed-off-by: Evgeny Slutsky <eslutsky@redhat.com>

---
+++ openstack/deploy_csi_driver_nfs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -ex
+
+nfs_server_ip=$1
+
+# Install latest csi-driver-nfs
+curl -skSL https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/v4.1.0/deploy/install-driver.sh | bash -s v4.1.0 --
+
+
+cat << EOF | kubectl apply -f -
+apiVersion: storage.k8s.io/v1
+kind: StorageClass
+metadata:
+  name: nfs-csi
+provisioner: nfs.csi.k8s.io
+parameters:
+  server: ${nfs_server_ip}
+  share: /home/nfsshare
+  subDir: nfs-csi  
+  # csi.storage.k8s.io/provisioner-secret is only needed for providing mountOptions in DeleteVolume
+  csi.storage.k8s.io/provisioner-secret-name: "mount-options"
+  csi.storage.k8s.io/provisioner-secret-namespace: "default"
+reclaimPolicy: Delete
+volumeBindingMode: Immediate
+mountOptions:
+EOF
+
--- openstack/setup.sh
+++ openstack/setup.sh
@@ -6,10 +6,13 @@ export ext_ip=$(ip route get 8.8.8.8 | awk '{ print $7 }' | head -1)
 # set the EXTERNAL_IP env for the packstack container,its needed for accessing the NFS.
 sed -i "s/<set_external_ip>/${ext_ip}/g" openstack/packstack_deployment.yml
 
-
 kubectl apply -f openstack/packstack_deployment.yml
 while ! kubectl get deployment -n konveyor-forklift packstack; do sleep 10; done
 kubectl wait deployment -n konveyor-forklift packstack --for condition=Available=True --timeout=280s
+
+# deploy csi-driver-nfs
+openstack/deploy_csi_driver_nfs.sh "${ext_ip}"
+
 
 sleep 5
 source openstack/utils.sh
