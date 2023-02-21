4d9f6147416a1ac9840b3a71442d16882ca3af9f
yzamir <yzamir@redhat.com>
2023-02-21 10:32:42 +0200

    Chack for podman when running setup manually
    Signed-off-by: yzamir <yzamir@redhat.com>

--- cluster/kind/kind_with_registry.sh
+++ cluster/kind/kind_with_registry.sh
@@ -6,17 +6,28 @@ set -o errexit
 
 go install sigs.k8s.io/kind@v0.15.0
 
+# add go bin to path
+echo "export PATH=$HOME/go/bin:$PATH" >> ~/.bashrc 
+source ~/.bashrc 
+
 [ "$(type -P kind)" ] || ( echo "kind is not in PATH" ;  exit 2 )
 
 
 mkdir -p /var/tmp/kind_storage
 chmod 777 /var/tmp/kind_storage
 
+# test for container command
+CONTAINER_CMD=docker
+if [ -x "$(type -P podman)" ]; then
+  CONTAINER_CMD=podman
+fi
+[ "$(type -P ${CONTAINER_CMD})" ] || ( echo "${CONTAINER_CMD} is not in PATH" ;  exit 2 )
+
 # create registry container unless it already exists
 reg_name='kind-registry'
 reg_port='5001'
-if [ "$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)" != 'true' ]; then
-  docker run \
+if [ "$(${CONTAINER_CMD} inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)" != 'true' ]; then
+  ${CONTAINER_CMD} run \
     -d --restart=always -p "127.0.0.1:${reg_port}:5000" -e REGISTRY_STORAGE_DELETE_ENABLED=true --name "${reg_name}" \
      --network bridge \
     registry:2
@@ -42,8 +53,8 @@ EOF
 kind get kubeconfig > /tmp/kubeconfig
 
 # connect the registry to the cluster network if not already connected
-if [ "$(docker inspect -f='{{json .NetworkSettings.Networks.kind}}' "${reg_name}")" = 'null' ]; then
-  docker network connect "kind" "${reg_name}"
+if [ "$(${CONTAINER_CMD} inspect -f='{{json .NetworkSettings.Networks.kind}}' "${reg_name}")" = 'null' ]; then
+  ${CONTAINER_CMD} network connect "kind" "${reg_name}"
 fi
 
 [ -z "${REMOTE_DOCKER_HOST}" ] || { setup_kind_sshtunnel  ; trap cleanup_kind_sshtunnel ERR ;  } 
