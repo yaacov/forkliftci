50b634f0bd68c24bc885c6b5ae87ebcd54cf6e8b
Arik Hadas <ahadas@redhat.com>
2023-02-28 12:11:52 +0200

    Use absolute path for OVIRT_CACERT
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- cluster/providers/ovirt/e2e_env_vars.sh
+++ cluster/providers/ovirt/e2e_env_vars.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 export OVIRT_USERNAME=admin@internal OVIRT_PASSWORD=123456 OVIRT_URL=https://fakeovirt.konveyor-forklift:30001/ovirt-engine/api
-export OVIRT_CACERT=./cluster/providers/ovirt/e2e_cacert.cer STORAGE_CLASS=standard OVIRT_VM_ID=31573c08-717b-43e0-825f-69a36fb0e1a1
+export OVIRT_CACERT=$(pwd)/cluster/providers/ovirt/e2e_cacert.cer STORAGE_CLASS=standard OVIRT_VM_ID=31573c08-717b-43e0-825f-69a36fb0e1a1
