da5265c0dcbb614216a7c736f8ff52b1d872481b
Arik Hadas <ahadas@redhat.com>
2023-03-05 21:55:33 +0000

    Set NFS also for vSphere test
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- cluster/providers/vmware/setup.sh
+++ cluster/providers/vmware/setup.sh
@@ -2,7 +2,14 @@
 
 set -ex
 
+[ -z "${NFS_IP_ADDRESS}" ] && { echo "Provider cannot be installed - NFS_IP_ADDRESS env required" ; return 2 ;}
+[ -z "${NFS_SHARE}" ] && { echo "Provider cannot be installed - NFS_SHARE env required" ; return 2 ;}
+
+
 kubectl apply -f ./cluster/providers/vmware/vcsim_deployment.yml
 
 while ! kubectl get deployment -n konveyor-forklift vcsim; do sleep 5; done
 kubectl wait deployment -n konveyor-forklift vcsim --for condition=Available=True --timeout=180s
+
+# deploy csi-driver-nfs if missing
+kubectl get StorageClass nfs-csi 2>/dev/null || cluster/providers/utils/deploy_csi_driver_nfs.sh "${NFS_IP_ADDRESS}" "${NFS_SHARE}"
