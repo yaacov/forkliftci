99c4906e000733ee759a5729e6ee95b66c6eedcb
Arik Hadas <ahadas@redhat.com>
2024-02-05 11:53:10 +0200

    invoke stub images tasks from root folder in forklift
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- build_tests_bazel.sh
+++ build_tests_bazel.sh
@@ -13,13 +13,13 @@ SCRIPT_DIR=`dirname "$SCRIPT_PATH"`
 
 
 # Change the dir to FORKLIFT_DIR (default forklift)
-cd ${FORKLIFT_DIR:-forklift}/virt-v2v/cold
+cd ${FORKLIFT_DIR:-forklift}
 
 [ ! -e "${SCRIPT_DIR}/stub-images" ] && { echo "stub-images not found";exit 2; }
 
 export REGISTRY_TAG=latest
 
 # Copy the stub-images under the bazel workspace
-cp -fr ${SCRIPT_DIR}/stub-images .
-bazel run //stub-images:push-forklift-virt-v2v-stub --verbose_failures
-bazel run //stub-images:push-vddk-test-vmdk --verbose_failures
+cp -fr ${SCRIPT_DIR}/stub-images virt-v2v/cold
+bazel run -package_path=virt-v2v/cold stub-images:push-forklift-virt-v2v-stub --verbose_failures
+bazel run -package_path=virt-v2v/cold stub-images:push-vddk-test-vmdk --verbose_failures
